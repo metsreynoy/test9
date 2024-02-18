a60617c1fbd31c2796cf9e4f2878700686ee79ec
metsreynoy <157475226+metsreynoy@users.noreply.github.com>
2024-02-19 06:28:11 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+/*const menu=document.querySelector('#menu-btn');
+const menu1=document.querySelector('.mobile-phone');
+menu1.addEventListener('click',function(){
+     menu.classList.toggle('');
+    // bar.classList.toggle('is');
+});*/
+ /*const menu=document.querySelector('.nav_bar ul');
+  const bar=documen.querySelector('#navbar_toggle');
+  bar.addEventListener('click',function(){
+     menu.classList.toggle('active');
+  }) 
+  // show menu
+ /* const navmenu=document.getElementById('');
+  const navtoggle=document.getElementById('');
+  const navclose=document.getElementById('');
+
+if(navToggle){
+     navtoggle.addEventListener('click',()=>{
+         navmenu.classList.add('show-menu')
+     })
+}
+/*Menu hidden*/
+/*if(navclose){
+   navclose.addEventListener('click',()=>{
+      navmenu.classList.remove('show-menu')
+   })
+}*/
+const menuLinks=document.querySelector('#menu-btn');
+const menu=document.querySelector('#menu-toggle');
+  menu.addEventListener('click',function(){
+           menuLinks.classList.toggle('active');
+  });
+
+  /*use silde*/
+ /*const clothes=document.querySelector('.clothes');
+
+  const left=document.querySelector('.left-side');
+   left.addEventListener('click',function(){
+           clothes.classList.toggle('.active');
+   });*/
+ 
+ 
+ 
+ const clothes=document.querySelector('.clothes-btn');
+
+  const left=document.querySelector('.left-side');
+   left.addEventListener('click',function(){
+           clothes.classList.toggle('active');
+              
+   });
+
+   
+  
+   
